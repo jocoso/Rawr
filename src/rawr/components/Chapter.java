12fd83439ba811ad6af86a677dc3369f43f393ea
joshua <joshua.collado022@gmail.com>
2019-10-08 17:24:20 -0400

    Document some classes, they are too many

--- src/rawr/components/Chapter.java
+++ src/rawr/components/Chapter.java
@@ -7,6 +7,15 @@ import java.util.List;
 import rawr.components.tools.TextUtilities;
 import rawr.components.utilities.CommandManager;
 
+
+/**
+ * Chapter class has all the tools to display the story
+ * in the GUI. Commands, Events, Narrative, Character creation...
+ * all goes in here.
+ * 
+ * @author Joshua Collado Soler
+ *
+ */
 public abstract class Chapter extends Entity{
 	// 
 		protected boolean finished;
@@ -35,20 +44,29 @@ public abstract class Chapter extends Entity{
 		}
 		
 		/**
-		 * Intro are the first thing that will be presented to the player
-		 * when they start a new chapter.
+		 * First thing to be presented to the player
+		 * when they start a new chapter of the game, followed by the 
+		 * name and the description of the protagonist whereabouts
 		 * @param intro
 		 */
 		public void setIntro(String intro) {
 			this.intro = intro;
 		}
 
+		/**
+		 * Returns the Chapter intro
+		 * 
+		 * @return intro
+		 */
 		public String getIntro() {
 			return intro;
 		}
 		
+		// TODO: Find out how to make the protagonist a mandatory field for Chapter
 		/**
-		 * Protagonist are the players at the moment. 
+		 * Protagonist are the player's skin. 
+		 * The protagonist contains very important information.
+		 * 
 		 * @param protagonist
 		 */
 		public void setProtagonist(Character protagonist) {
@@ -56,46 +74,71 @@ public abstract class Chapter extends Entity{
 		}
 
 		/**
-		 * Commands are interfaces created to modify a piece of data inside the 
-		 * Chapter class. 
+		 * Takes one command and ensures that it gets executed when
+		 * the user input its name
+		 * 
+		 * @param command_name 
+		 * @param command 
 		 */
 		public void addCommand(String command_name, Command command) {
-			if(command == null || command_name == null) {
-				System.err.println("Error: none of the parameters can be null");
-				System.exit(1);
-			}
 			String c = TextUtilities.oneWordSanitizer(command_name);
 			cm.addCommand(c, command);
 		}
-
+		
+		/**
+		 * Takes an array of commands and ensures that they get executed when
+		 * the user input their name.</br> </br>
+		 * 
+		 * <b>NOTE:</b> The index on the command_names should be the same as the respective index
+		 * of their commands counterpart
+		 * 
+		 * @param command_names 
+		 * @param commands 
+		 */
 		public void addCommands(String[] command_names, Command[] commands) {
 			cm.addCommands(command_names, commands);
 		}
 
+		/**
+		 * Add a map to the chapter for Book to manage information more efficiently.
+		 * @param map 
+		 */
 		protected void addMap(GameMap map) {
 			// TODO: Create an ErrorHandler for this case
 			if (map == null)
 				return;
 			this.maps.add(map);
 		}
-
+		
+		/**
+		 * Add multiple maps to the chapter for Book to manage information more efficiently.
+		 * @param maps 
+		 */
 		protected void addMaps(GameMap[] maps) {
 			for (GameMap map : maps)
 				addMap(map);
 		}
-
-		public void setFinished(boolean finished) {
-			this.finished = finished;
+		
+		/**
+		 * A chapter must finished in an event. In this event, the chapter
+		 * is notified it is finished by using this method. 
+		 */
+		public void setFinished() {
+			this.finished = true;
 		}
-
+		
+		/**
+		 * Book will execute this method to know when to move on to the next chapter
+		 * @return Boolean
+		 */
 		public boolean hasFinished() {
 			return finished;
 		}
 		
 		/**
-		 * Events are interfaces created to keep looking for certain conditions
-		 * Events can run many times but once they server their purpose they are
-		 * eliminated from the queue
+		 * Add an Event to the queue for the Book to execute it 
+		 * every iteration.
+		 * @param event
 		 */
 		protected void addEvent(Event event) {
 			// TODO: Create an ErrorHandler for this case
@@ -105,14 +148,22 @@ public abstract class Chapter extends Entity{
 			}
 			events.add(event);
 		}
-
+		
+		/**
+		 * Add an array of events to the queue for the Book to execute them
+		 * every iteration.
+		 * @param events 
+		 */
 		protected void addEvents(Event[] events) {
 			for (Event event : events)
 				addEvent(event);
 		}
 
 		
-
+		/**
+		 * Removes an event from the queue. 
+		 * @param event
+		 */
 		protected void removeEvent(Event event) {
 			if (event == null) {
 				System.out.println("ERROR: Event is null");
@@ -121,6 +172,10 @@ public abstract class Chapter extends Entity{
 			events.remove(event);
 		}
 
+		/**
+		 * Check all the events in queue. If one return true
+		 * eliminate it from queue.
+		 */
 		protected void checkEvents() {
 			Iterator<Event> iter = events.iterator();
 
@@ -132,10 +187,19 @@ public abstract class Chapter extends Entity{
 			}
 		}
 		
+		/**
+		 * Returns the protagonist of the story. 
+		 * 
+		 * @return Person protagonist
+		 */
 		public Person getProtagonist() {
 			return protagonist;
 		}
 		
+		/**
+		 * Overridable method. 
+		 * The Library user will create or execute their stories in here.
+		 */
 		public abstract void init();
 
 
--- src/rawr/components/ConsoleBook.java
+++ src/rawr/components/ConsoleBook.java
@@ -3,7 +3,7 @@ package rawr.components;
 /**
  * Books are made to micromanage Chapters in a way it is easier to understand
  * 
- * @author Temp
+ * @author Joshua Collado Soler
  *
  */
 public abstract class ConsoleBook extends Book {
@@ -11,20 +11,6 @@ public abstract class ConsoleBook extends Book {
 	public ConsoleBook(String name, String description) {
 		super(name, description);
 		init();
-	}
-
-	/**
-	 * Verify the book was created thoughtfully before start using it or its
-	 * components
-	 * 
-	 * @return true if the book was created correctly false otherwise
-	 */
-	public boolean isReady() {
-		return hasChapters();
-	}
-
-	public boolean hasChapters() {
-		return !chapters.isEmpty();
 	}
 
 	@Override
@@ -53,6 +39,11 @@ public abstract class ConsoleBook extends Book {
 
 	}
 	
+	/**
+	 * Get next chapter in line
+	 * 
+	 * @return ConsoleChapter
+	 */
 	public ConsoleChapter getNextInLine() {
 		return (ConsoleChapter) chapters.poll();
 	}
